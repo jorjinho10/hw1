21d2a9e64eaa400f586a4f8184a476237047177d
George Yakovlev <jorjinho10@mail.ru>
2016-06-07 21:57:10 +0300

    task 2 first try 1.1

--- src/yakovlev/hw01_02/Maintwo.java
+++ src/yakovlev/hw01_02/Maintwo.java
@@ -6,11 +6,13 @@ package yakovlev.hw01_02;
 public class Maintwo {
 
     public static void main(String[] args) {
-      double [] a;
+
+        double [] a;
         a = new double [10];
         int epsilon=2;
         for (int i=0; a[i]<epsilon ;i++) {
             a[i]= (double) ((1.0)/((i + 1)*(i + 1)));
+            a[i]= Math.round(a[i] * 10000.0) / 10000.0;
             System.out.println(i+"-ый элемент массива а = "+a[i]);
         }
         System.out.println("Наименьший номер элемента последовательности" + a[1]);
