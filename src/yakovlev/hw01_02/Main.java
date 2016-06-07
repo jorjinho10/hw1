6f2d53afa813d8384f1daee37cc4815f5f922c60
George Yakovlev <jorjinho10@mail.ru>
2016-06-07 21:32:12 +0300

    task 2 first try

--- src/yakovlev/hw01_02/Main.java
+++
@@ -1,7 +0,0 @@
-package yakovlev.hw01_02;
-
-/**
- * Created by Администратор on 05.06.2016.
- */
-public class Main {
-}
---
+++ src/yakovlev/hw01_02/Maintwo.java
@@ -0,0 +1,19 @@
+package yakovlev.hw01_02;
+
+/**
+ * Created by Администратор on 07.06.2016.
+ */
+public class Maintwo {
+
+    public static void main(String[] args) {
+      double [] a;
+        a = new double [10];
+        int epsilon=2;
+        for (int i=0; a[i]<epsilon ;i++) {
+            a[i]= (double) ((1.0)/((i + 1)*(i + 1)));
+            System.out.println(i+"-ый элемент массива а = "+a[i]);
+        }
+        System.out.println("Наименьший номер элемента последовательности" + a[1]);
+    }
+
+}
