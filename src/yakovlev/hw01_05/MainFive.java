d3139877e843a0d62c4f74233030acb2d3c0879e
George Yakovlev <jorjinho10@mail.ru>
2016-06-10 00:50:54 +0300

    ready modules: 1,2,5

--- src/yakovlev/hw01_05/MainFive.java
+++ src/yakovlev/hw01_05/MainFive.java
@@ -9,26 +9,26 @@ public class MainFive {
 
         HelloFive hi = new HelloFive();
         hi.hello(); //Приветствие и постановка задачи
-        System.out.println("Введите размерность матрицы: ");
+        System.out.println("Введите размерность квадратной матрицы: ");
         Scanner input = new Scanner(System.in);
         if( input.hasNextInt() )
-        {// Считываем с клавиатуры целое число
+        {// Считываем с клавиатуры размерность матрицы
             matrsize = input.nextInt();}
 
         int [][] a;
         a = new int [matrsize][matrsize];
         int b = a.length;
         for (int i=0; i<a.length; i++) {
-            b--;
+            b--; //встречное считывание для второй косой черты справа сверху налево вниз
             for (int j=0; j<matrsize; j++) {
-                if (i==j) {a[i][j]=1;}
+                if (i==j) {a[i][j]=1;} //проверка на элемент основной диагонали
                 else {
-                    if (j==b) {a[i][j]=1;}
+                    if (j==b) {a[i][j]=1;} //проверка на элемент второй диагонали
                     else {a[i][j]=0;}
                 };
-                System.out.print(" "+a[i][j]+" ");
+                System.out.print(" "+a[i][j]+" "); //вывод строки
             }
-            System.out.print('\n');
+            System.out.print('\n'); //enter
         }
     }
 
