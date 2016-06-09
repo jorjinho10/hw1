f81042b37ac7a6ec2e352a74f0adcaaf1844f4df
George Yakovlev <jorjinho10@mail.ru>
2016-06-09 21:10:02 +0300

    2/6 ready

--- src/yakovlev/hw01_02/Maintwo.java
+++ src/yakovlev/hw01_02/Maintwo.java
@@ -10,17 +10,17 @@ public class Maintwo {
         int minind=-1000;
 
         Hellotwo hi = new Hellotwo();
-        hi.hello();
+        hi.hello(); //Приветствие и постановка задачи
         System.out.println("Введите число элементов последовательности: ");
         Scanner input = new Scanner(System.in);
         if( input.hasNextInt() )
-           {// Читаем с клавиатуры целое число
+           {// Считываем с клавиатуры целое число
                 massize = input.nextInt();}
 
         System.out.println("Введите положительный эпсилон (разделитель ','): ");
         Scanner inputt = new Scanner(System.in);
         if( inputt.hasNextDouble() )
-        {// Читаем с клавиатуры эпсилон
+        {// Считываем с клавиатуры эпсилон (double)
             epsilon = inputt.nextDouble();}
 
         double [] a;
@@ -30,12 +30,12 @@ public class Maintwo {
         for (int i=0; i<a.length ;i++) {
             n=i+1;
             a[i]= ((1.0)/((n + 1)*(n + 1)));
-            a[i]= Math.round(a[i] * 10000.0) / 10000.0;
+            a[i]= Math.round(a[i] * 10000.0) / 10000.0; //округление до 4-х знаков после запятой
             System.out.println(n+"-ый элемент массива а = "+a[i]);
             //System.out.println("Введенное epsilon: "+ epsilon);
-            if (a[i]<epsilon) {
-                minind=n;
-                epsilon=-100;
+            if (a[i]<epsilon) { // проверка на условие задачи
+                minind=n; //запоминаем индекс первого жлемента, удовлетворяющего условию
+                epsilon=-100; // для выполнения 1 раз
             }
         }
         System.out.println("Наименьший номер элемента ("+a[minind-1]+") удовлетворяющего условию: № "+ minind);
