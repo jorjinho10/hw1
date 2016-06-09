0122ed95fcf73c433dddbec4cbe98f7a953ee89e
George Yakovlev <jorjinho10@mail.ru>
2016-06-09 19:36:07 +0300

    task 2 first try 2

--- src/yakovlev/hw01_02/Maintwo.java
+++ src/yakovlev/hw01_02/Maintwo.java
@@ -1,21 +1,42 @@
 package yakovlev.hw01_02;
+import java.util.Scanner;
 
-/**
- * Created by Администратор on 07.06.2016.
- */
 public class Maintwo {
 
     public static void main(String[] args) {
+        int massize=0;
+        double epsilon=0.0;
+        int minind=-1000;
+
+        System.out.print("Введите число элементов последовательности: ");
+        Scanner input = new Scanner(System.in);
+        if( input.hasNextInt() )
+           {// Читаем с клавиатуры целое число
+                massize = input.nextInt();}
+
+        System.out.print("Введите эпсилон: ");
+        Scanner inputt = new Scanner(System.in);
+        if( inputt.hasNextDouble() )
+        {// Читаем с клавиатуры эпсилон
+            epsilon = input.nextDouble();}
 
         double [] a;
-        a = new double [10];
-        int epsilon=2;
-        for (int i=0; a[i]<epsilon ;i++) {
-            a[i]= (double) ((1.0)/((i + 1)*(i + 1)));
+        a = new double [massize];
+        int n=0;
+
+        for (int i=0; i<a.length ;i++) {
+            n=i+1;
+            a[i]= ((1.0)/((n + 1)*(n + 1)));
             a[i]= Math.round(a[i] * 10000.0) / 10000.0;
-            System.out.println(i+"-ый элемент массива а = "+a[i]);
+            System.out.println(n+"-ый элемент массива а = "+a[i]);
+            System.out.println("Введенное epsilon: "+ epsilon);
+            if (a[i]<epsilon) {
+                minind=n;
+                epsilon=-100;
+            }
         }
-        System.out.println("Наименьший номер элемента последовательности" + a[1]);
+        System.out.println("Введенное число: "+ massize);
+        System.out.println("Номер наименьшего "+ minind);
     }
 
 }
